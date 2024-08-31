b039227df96c625a256a18e3e490cf27362f02be
guilopes2020 <guilopesdev@gmail.com>
2024-08-31 19:37:03 -0300

    feat: :sparkles: validations

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,14 +1,27 @@
 using System.ComponentModel.DataAnnotations;
+using TWTodosAspNet.Validators;
 
 namespace TWTodosAspNet.Models;
 
 public class Todo
 {
     public int Id { get; set; }
+
+    [Display(Name = "Título")]
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caractéres")]
     public string Title { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
 
-    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+    [Display(Name = "Data de entrega")]
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    [FutureOrPresent]
     public DateOnly Deadline { get; set; }
     public DateOnly? FinishedAt { get; set; }
+
+    public void Finish()
+    {
+        FinishedAt = DateOnly.FromDateTime(DateTime.Now);
+    }
 }
