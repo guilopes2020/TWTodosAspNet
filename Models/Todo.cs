57fd521e4073b47da7fb8b2105f232404d96c691
guilopes2020 <guilopesdev@gmail.com>
2024-08-31 15:53:02 -0300

    feat: :sparkles: registering-and-receiving-tasks

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TWTodosAspNet.Models;
 
 public class Todo
@@ -5,6 +7,8 @@ public class Todo
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
     public DateOnly Deadline { get; set; }
     public DateOnly? FinishedAt { get; set; }
 }
